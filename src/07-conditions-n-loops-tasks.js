33a6971ba365c3c19e048026fb237ca2ed191808
Vladimir <vladimirkukolovich@gmail.com>
2022-07-26 00:07:46 +0300

    70

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -263,23 +263,24 @@ function reverseInteger(num) {
  *   5436468789016589 => false
  *   4916123456789012 => false
  */
-function isCreditCardNumber(ccn) {
-  if (/[^0-9-\s]+/.test(ccn)) return false;
+function isCreditCardNumber(/* ccn */) {
+//   if (/[^0-9-\s]+/.test(ccn)) return false;
 
 
-  let nCheck = 0;
-  let bEven = false;
-  ccn = String(ccn).replace(/\D/g, '');
-  for (let n = ccn.length - 1; n >= 0; n--) {
-    const cDigit = ccn.charAt(n);
-    let nDigit = parseInt(cDigit, 10);
-    if (bEven) {
-      if ((nDigit *= 2) > 9) nDigit -= 9;
-    }
-    nCheck += nDigit;
-    bEven = !bEven;
-  }
-  return (nCheck % 10) === 0;
+  //   let nCheck = 0;
+  //   let bEven = false;
+  //   ccn = String(ccn).replace(/\D/g, '');
+  //   for (let n = ccn.length - 1; n >= 0; n--) {
+  //     const cDigit = ccn.charAt(n);
+  //     let nDigit = parseInt(cDigit, 10);
+  //     if (bEven) {
+  //       if ((nDigit *= 2) > 9) nDigit -= 9;
+  //     }
+  //     nCheck += nDigit;
+  //     bEven = !bEven;
+  //   }
+  //   return (nCheck % 10) === 0;
+  throw new Error('Not implemented');
 }
 
 /**
