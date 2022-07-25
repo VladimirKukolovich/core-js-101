c81dad9a5e0d2aae49dd5c57f6e6e325b5b765e6
Vladimir <vladimirkukolovich@gmail.com>
2022-07-26 00:31:23 +0300

    add: 71

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -133,6 +133,7 @@ function doRectanglesOverlap(rect1, rect2) {
   return Boolean(rect1.left + rect1.width >= rect2.left && rect1.top + rect1.height >= rect2.top);
 }
 
+
 /**
  * Returns true, if point lies inside the circle, otherwise false.
  * Circle is an object of
