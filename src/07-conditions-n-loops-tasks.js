ecdb03dc94a87c469f1d5a1d2e194fd63dec2834
Vladimir <vladimirkukolovich@gmail.com>
2022-07-26 00:39:53 +0300

    add: 23

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -130,9 +130,9 @@ function isTriangle(/* a, b, c */) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-// return Boolean(rect1.left + rect1.width >= rect2.left && rect1.top + rect1.height >= rect2.top);
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  return Boolean(rect1.left + rect1.width >= rect2.left && rect1.top + rect1.height >= rect2.top);
+  // throw new Error('Not implemented');
 }
 
 /**
